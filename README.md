ab5c9104365170e88f3440dd2c5d511cb0eceaf2
Ky Lucero <kileelucero@gmail.com>
2020-12-09 14:55:32 -0700

    refactored files and folders and updated the ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # code-quiz
+![License: none](https://img.shields.io/badge/License-none-blue.svg)
+## Description
+Fun Little Web Quiz To Test Knowledge Of JavaScript
+
+Link to live site: [code-quiz](https://kileelucero.github.io/code-quiz/)
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution](#contribution)
+* [Tests](#tests)
+* [License](#license)
+* [Questions](#questions)
+
+## Installation
+Run In Browser
+## Usage
+Used To Play A Quick Quiz Game
+## License
+none
+Copyright (c) 2020 Kilee Lucero
+## Contribution
+When contributing to this repository, please first discuss the change you wish to make via issue, email, or any other method with the owner(s) of this repository before making a change.
+## Tests
+none
+## Questions
+For any questions the author can be contacted at:
+GitHub: @[kileelucero](https://github.com/kileelucero)
+Email: kileelucero@gmail.com
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     //hidden questions to begin the quiz
-    $("div#jumbo2,#jumbo3,#jumbo4,#jumbo5,#jumbo6,#jumbo7").hide();
+    $("div#jumbo2,#jumbo3,#jumbo4,#jumbo5,#jumbo6,#jumbo7,#jumbo8,#rightWrong,#rightWrong1,#rightWrong2,#rightWrong3,#rightWrong4").hide();
 
     //variables referred to back whenever
     var highScore = 0;
-    var newHighScore = "";
     var timerCount = 60;
+    var newTimerCount;
 
     //Alert for user to select start button to begin timer
     alert("Please click 'Begin Quiz' button to start timer before answering question and best of luck!")
@@ -13,15 +13,16 @@ $(document).ready(function () {
     //Code for Question One
     $("#c").on("click", function () {
         highScore = highScore + 7;
-        // highScore = newHighScore;
         $("#highScoreTally").html(highScore);
+        $()
         $("#jumbo1").hide();
         $("#jumbo2").show();
+        $("")
     })
 
     $("#b,#a").on("click", function () {
         highScore = highScore - 5;
-        // highScore = newHighScore;
+        newTimerCount = timerCount - 3;
         $("#highScoreTally").html(highScore);
         $("#jumbo1").hide();
         $("#jumbo2").show();
@@ -29,7 +30,7 @@ $(document).ready(function () {
 
     $("#d").on("click", function () {
         highScore = highScore + 0;
-        // highScore = newHighScore;
+        newTimerCount = timerCount - 3;
         $("#highScoreTally").html(highScore);
         $("#jumbo1").hide();
         $("#jumbo2").show();
@@ -39,7 +40,6 @@ $(document).ready(function () {
     //Code for Question two
     $("#a1").on("click", function () {
         highScore = highScore + 7;
-        // highScore = newHighScore;
         $("#highScoreTally").html(highScore);
         $("#jumbo2").hide();
         $("#jumbo3").show();
@@ -47,7 +47,7 @@ $(document).ready(function () {
 
     $("#b1,#c1").on("click", function () {
         highScore = highScore - 5;
-        // highScore = newHighScore;
+        newTimerCount = timerCount - 3;
         $("#highScoreTally").html(highScore);
         $("#jumbo2").hide();
         $("#jumbo3").show();
@@ -55,7 +55,7 @@ $(document).ready(function () {
 
     $("#d1").on("click", function () {
         highScore = highScore + 0;
-        // highScore = newHighScore;
+        newTimerCount = timerCount - 3;
         $("#highScoreTally").html(highScore);
         $("#jumbo2").hide();
         $("#jumbo3").show();
@@ -64,7 +64,6 @@ $(document).ready(function () {
     //Code for Question three
     $("#b2").on("click", function () {
         highScore = highScore + 7;
-        // highScore = newHighScore;
         $("#highScoreTally").html(highScore);
         $("#jumbo3").hide();
         $("#jumbo4").show();
@@ -72,7 +71,7 @@ $(document).ready(function () {
 
     $("#a2,#c2").on("click", function () {
         highScore = highScore - 5;
-        // highScore = newHighScore;
+        newTimerCount = timerCount - 3;
         $("#highScoreTally").html(highScore);
         $("#jumbo3").hide();
         $("#jumbo4").show();
@@ -80,7 +79,7 @@ $(document).ready(function () {
 
     $("#d2").on("click", function () {
         highScore = highScore + 0;
-        // highScore = newHighScore;
+        newTimerCount = timerCount - 3;
         $("#highScoreTally").html(highScore);
         $("#jumbo3").hide();
         $("#jumbo4").show();
@@ -89,7 +88,6 @@ $(document).ready(function () {
     //Code for Question four
     $("#d3").on("click", function () {
         highScore = highScore + 7;
-        // highScore = newHighScore;
         $("#highScoreTally").html(highScore);
         $("#jumbo4").hide();
         $("#jumbo5").show();
@@ -98,7 +96,7 @@ $(document).ready(function () {
 
     $("#a3,#c3,#b3").on("click", function () {
         highScore = highScore - 5;
-        // highScore = newHighScore;
+        newTimerCount = timerCount - 3;
         $("#highScoreTally").html(highScore);
         $("#jumbo4").hide();
         $("#jumbo5").show();
@@ -106,57 +104,84 @@ $(document).ready(function () {
 
     //Code for Question five
     $("#b4").on("click", function () {
-        var timerInterval = setInterval(function () {
-            clearInterval(timerInterval);
-        })
         highScore = highScore + 7;
-        // highScore = newHighScore;
         $("#highScoreTally").html(highScore);
         $("#jumbo5").hide();
-        $("#jumbo7").show();
-        clearInterval(timerInterval);
+        endQuiz();
     })
 
     $("#a4,#c4").on("click", function () {
-        var timerInterval = setInterval(function () {
-            clearInterval(timerInterval);
-        })
         highScore = highScore - 5;
-        // highScore = newHighScore;
+        newTimerCount = timerCount - 3;
         $("#highScoreTally").html(highScore);
         $("#jumbo5").hide();
-        $("#jumbo7").show();
-        clearInterval(timerInterval);
+        endQuiz();
     })
 
     $("#d4").on("click", function () {
-        var timerInterval = setInterval(function () {
-            clearInterval(timerInterval);
-        })
         highScore = highScore + 0;
-        // highScore = newHighScore;
+        newTimerCount = timerCount - 3;
         $("#highScoreTally").html(highScore);
         $("#jumbo5").hide();
-        $("#jumbo7").show();
+        endQuiz();
     })
-
+    
     //Code for high score
     $("#finalScore").on("click", function () {
-        get(localStorage)
+        highScore = localStorage.getItem("High-Score")
+        $("#highScoreTally").html(highScore)
     })
+    
+    //code to end quiz and timer
+    function endQuiz() {
+        $("#jumbo7").show();
+        clearInterval(newTimerCount);
+        localStorage.setItem(".high-score", highScore)
+        $(".high-score").html(highScore)
+    }
+    
+    //code for timer countdown
+    function timerCounter () {
+        timerCount--;
+        $("#timerTally").html("Time Left: " + timerCount);
+        if (timerCount <=0) {
+            timerCount = 0;
+            endQuiz();
+        }
+    }
 
-    //code for timer
-    $(".start").on("click", function () {
-        var timerInterval = setInterval(function () {
-            timerCount--;
-            $("#timerTally").html("Time Left: " + timerCount);
-
-            if (timerCount === 0) {
-                clearInterval(timerInterval);
-            }
-
-        }, 1000)
+    //code for timer to begin
+    function beginQuiz() {
+        newTimerCount = setInterval(timerCounter, 1000);
         $(".start").fadeOut();
+    }
+
+    //code for start button
+    $(".start").on("click", function () {
+        $(".start").fadeOut();
+        beginQuiz();
     })
 
+    //code for submit button
+    $("#finalScore").on("click", function() {
+        $('#initialsInput[type="submit"]').each(function(){
+            var finalScore = $(this).attribute("#finalScore");
+            var value = $(this).value();
+            localStorage.setItem(finalScore, value);
+        })
+        ("#jumbo7").hide();
+        ("#jumbo8").show();
+    })
+
+    $("#finalScore").on("click", function() {
+        $('#initialsInput[type="submit"]').each(function(){
+            var finalScore = $(this).attribute("#finalScore");
+            var value = localStorage.getItem(finalScore);
+            $(this).val(value);
+        }); 
+        ("#jumbo7").hide();
+        ("#jumbo8").show();
+    });
+
+    
 });
